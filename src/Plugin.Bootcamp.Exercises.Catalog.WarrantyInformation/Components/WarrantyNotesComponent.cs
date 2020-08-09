349a26bfd2698403fed2d50727f6c73f3193280e
GuidovTricht <guidovantricht@hotmail.com>
2020-08-09 21:17:21 +0000

    Lab 2 started

--- src/Plugin.Bootcamp.Exercises.Catalog.WarrantyInformation/Components/WarrantyNotesComponent.cs
+++ src/Plugin.Bootcamp.Exercises.Catalog.WarrantyInformation/Components/WarrantyNotesComponent.cs
@@ -5,7 +5,7 @@ namespace Plugin.Bootcamp.Exercises.Catalog.WarrantyInformation.Components
     public class WarrantyNotesComponent : Component
     {
         /* STUDENT: Add properties as specified in the requirements */
-        public string WarrantyType { get; set; }
-        public int WarrantyTerm { get; set; }
+        public string WarrantyType { get; set; } //Gold or Silver
+        public int WarrantyTerm { get; set; } //Years; 1, 2 or 3
     }
 }
--- src/Plugin.Bootcamp.Exercises.Catalog.WarrantyInformation/Pipelines/Blocks/GetWarrantyNotesViewBlock.cs
+++ src/Plugin.Bootcamp.Exercises.Catalog.WarrantyInformation/Pipelines/Blocks/GetWarrantyNotesViewBlock.cs
@@ -13,12 +13,42 @@ namespace Plugin.Bootcamp.Exercises.Catalog.WarrantyInformation.Pipelines.Blocks
     [PipelineDisplayName("GetWarrantyNotesViewBlock")]
     public class GetWarrantyNotesViewBlock : PipelineBlock<EntityView, EntityView, CommercePipelineExecutionContext>
     {
+        private readonly ViewCommander _viewCommander;
+
+        public GetWarrantyNotesViewBlock(ViewCommander viewCommander)
+        {
+            this._viewCommander = viewCommander;
+        }
+
         public override Task<EntityView> Run(EntityView arg, CommercePipelineExecutionContext context)
         {
             Condition.Requires(arg).IsNotNull($"{Name}: The argument cannot be null.");
             var catalogViewsPolicy = context.GetPolicy<KnownCatalogViewsPolicy>();
 
             /* STUDENT: Complete the Run method as specified in the requirements */
+            var isVariantView = arg.Name.Equals(catalogViewsPolicy.Variant);
+            var isMasterView = arg.Name.Equals(catalogViewsPolicy.Master);
+            var isWarrantyNotesView = arg.Name.Equals("WarrantyNotes");
+            var entityViewArgument = _viewCommander.CurrentEntityViewArgument(context.CommerceContext);
+            var sellableItem = entityViewArgument?.Entity as SellableItem;
+
+            if(sellableItem != null || (!isVariantView && !isMasterView) || !isWarrantyNotesView)
+            {
+                var variationId = string.Empty;
+                if (!string.IsNullOrEmpty(arg.ItemId))
+                {
+                    variationId = arg.ItemId;
+                }
+
+                var isEditView = arg.Action.Equals("WarrantyNotes-Edit");
+                if (!isEditView)
+                {
+                    arg.ChildViews.Add(new EntityView()
+                    {
+
+                    })
+                }
+            }
 
             return Task.FromResult(arg);
         }
